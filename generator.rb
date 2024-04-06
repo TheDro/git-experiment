f30842b639959a236bbaa63705ae3041c9aefbf2
TheDro <andrewsc32@protonmail.com>
2024-04-06 09:14:50 -0400

    Hahaha, it works

--- generator.rb
+++ generator.rb
@@ -8,3 +8,17 @@ def generate(n)
     end
     # `git push`
 end
+
+def generate_at(date)
+    filename = "files/#{Random.rand(1e9).to_i}.txt"
+    File.write(filename, "Hello World!")
+    `git add #{filename}`
+    `git commit -m "Added #{filename}" --date "#{date}"`
+    `git push`
+end
+
+# generate_at(Time.new(2024, 3, 12, 8, 30))
+# generate_at(Time.new(2024, 3, 19, 8, 30))
+# generate_at(Time.new(2024, 3, 20, 8, 30))
+# generate_at(Time.new(2024, 3, 21, 8, 30))
+# generate_at(Time.new(2024, 3, 14, 8, 30))
