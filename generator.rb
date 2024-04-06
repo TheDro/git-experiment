abfbcb8b23eedd5f76dd785e0aaf1af0ebf2e55f
TheDro <andrewsc32@protonmail.com>
2024-04-06 09:06:22 -0400

    Removed git commands from generator

--- generator.rb
+++ generator.rb
@@ -3,8 +3,8 @@ def generate(n)
     (1..n).each do |i|
         filename = "files/#{Random.rand(1e9).to_i}.txt"
         File.write(filename, "Hello World!")
-        `git add #{filename}`
-        `git commit -m "Added #{filename}"`
+        # `git add #{filename}`
+        # `git commit -m "Added #{filename}"`
     end
-    `git push`
+    # `git push`
 end
